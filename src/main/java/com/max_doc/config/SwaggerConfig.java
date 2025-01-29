8495738253b73e31539fe36c34629553ec61d836
Davii-Code <davifarigoncalves@gmail.com>
2025-01-28 22:34:08 -0300

    fix/configuração swagger

---
+++ src/main/java/com/max_doc/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.max_doc.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API Documentation")
+                        .description("API for managing documents")
+                        .version("v1"));
+    }
+}
